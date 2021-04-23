58aff30366b5d67d1949c46292029e76f39cadd0
Adrian SanPedro <adrian.r.sanpedro@gmail.com>
2021-04-23 11:00:20 -0700

    Dev-Server: Init Express server with Hello World

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 8081;
+
+app.use(cors());
+
+app.get('/', (request, response) => {
+    response.status(200).send('Hello world');
+});
+
+app.listen(port, () => {
+    console.log(`Moves Reference Guide listening at http://localhost:${port}`);
+});
